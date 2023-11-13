66d37fd2d2f00117bbf2eb77eccf3e8aa52992b7
Brent Westbrook <brentrwestbrook@gmail.com>
2023-11-13 09:50:52 -0500

    successfully scan the whole test set

--- src/smarts/scanner.rs
+++ src/smarts/scanner.rs
@@ -2,14 +2,25 @@ use std::{iter::Peekable, str::Chars};
 
 #[derive(Debug)]
 enum Token {
+    // punctuation
     LBrack,
     RBrack,
+    LParen,
+    RParen,
     Colon,
+    Dash, // could be bond or charge at this point
+    At,
+    // counts
     Atom(usize),
     HCount(usize),
     Digit(usize),
-    SingleBond,
+    Plus(usize),
+    // bonds
+    DoubleBond,
     TripleBond,
+    UpBond,
+    DownBond,
+    // end
     End,
 }
 
@@ -29,6 +40,14 @@ fn scan(s: String) -> Vec<Token> {
         let got = match c {
             '[' => T::LBrack,
             ']' => T::RBrack,
+            '(' => T::LParen,
+            ')' => T::RParen,
+            ':' => T::Colon,
+            '-' => T::Dash,
+            '@' => T::At,
+            '=' => T::DoubleBond,
+            '\\' => T::DownBond,
+            '/' => T::UpBond,
             '#' => {
                 // # can either be a number inside of an atom, eg [#6], or a
                 // triple bond. at some point we might have to improve this
@@ -41,21 +60,37 @@ fn scan(s: String) -> Vec<Token> {
                 }
             }
             'H' => T::HCount(get_digits(&mut chars).parse().unwrap_or(1)),
-            ':' => T::Colon,
+            '+' => T::Plus(get_digits(&mut chars).parse().unwrap_or(1)),
             '0'..='9' => T::Digit(
                 // combine the digit in c with any following digits
                 format!("{c}{}", get_digits(&mut chars)).parse().unwrap(),
             ),
-            '-' => T::SingleBond,
-            _ => panic!("unrecognized token {c}"),
+            _ => panic!("unrecognized token {c} in \n{s}"),
         };
         ret.push(got);
     }
     ret
 }
 
-#[test]
-fn test_scan() {
-    let s = r#"[#6H3:1]-[#6H2:2]-[#7H:3]-[#7H:4]-[#6H3:5]"#;
-    dbg!(scan(s.to_owned()));
+#[cfg(test)]
+mod tests {
+    use crate::{rdkit::to_smarts, Dataset};
+
+    use super::*;
+
+    #[test]
+    fn simple_scan() {
+        let s = r#"[#6H3:1]-[#6H2:2]-[#7H:3]-[#7H:4]-[#6H3:5]"#;
+        scan(s.to_owned());
+    }
+
+    #[test]
+    fn big_scan() {
+        let mut smiles =
+            Dataset::load("testfiles/opt.json").unwrap().to_smiles();
+        smiles.dedup();
+        for smile in smiles {
+            scan(to_smarts(smile));
+        }
+    }
 }
