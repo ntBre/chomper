31dc0174e8135ca870b523a2182da11f4b53da77
Brent Westbrook <brentrwestbrook@gmail.com>
2024-05-02 13:34:57 -0400

    bring up to date with new pyo3 version

--- src/rdkit.rs
+++ src/rdkit.rs
@@ -1,8 +1,8 @@
-use pyo3::{types::PyModule, Python};
+use pyo3::{prelude::PyAnyMethods, types::PyModule, Python};
 
 pub fn to_smarts(smiles: String) -> String {
     Python::with_gil(|py| {
-        let chem = PyModule::import(py, "rdkit.Chem").unwrap();
+        let chem = PyModule::import_bound(py, "rdkit.Chem").unwrap();
         let mol = chem.call_method1("MolFromSmiles", (smiles,)).unwrap();
         chem.call_method1("MolToSmarts", (mol, true))
             .unwrap()
